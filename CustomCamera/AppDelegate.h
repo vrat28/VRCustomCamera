9b940e6babdb0efa664d9503686324649c98d2de
Varun Rathi <varun.rathi@jeevansathi.com>
2019-04-09 13:12:57 +0530

    Initial Commit

---
+++ CustomCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCamera
+//
+//  Created by Varun Rathi on 09/04/19.
+//  Copyright © 2019 Varun Rathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
