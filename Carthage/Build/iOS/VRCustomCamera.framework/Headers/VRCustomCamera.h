d880a6b38c6e1ce3d7494f71cc6cd772d7b13c2a
Varun Rathi <varun.rathi@jeevansathi.com>
2019-04-23 10:52:12 +0530

    carthage added

---
+++ Carthage/Build/iOS/VRCustomCamera.framework/Headers/VRCustomCamera.h
@@ -0,0 +1,19 @@
+//
+//  VRCustomCamera.h
+//  VRCustomCamera
+//
+//  Created by Varun Rathi on 22/04/19.
+//  Copyright © 2019 Varun Rathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VRCustomCamera.
+FOUNDATION_EXPORT double VRCustomCameraVersionNumber;
+
+//! Project version string for VRCustomCamera.
+FOUNDATION_EXPORT const unsigned char VRCustomCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VRCustomCamera/PublicHeader.h>
+
+
